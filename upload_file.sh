4d5e165fca3fda72975c0422e190f476905ce9a8
Daniel White <97danny.white@gmail.com>
2017-11-24 17:24:12 -0800

    small changes also sensitive data not on git

--- upload_file.sh
+++ upload_file.sh
@@ -1,8 +1,13 @@
 # this checks the mail, and if a signal has come in
 # then upload the specified file
 # signal out is a unix file path and nothing more
+SHELL=/bin/bash
+PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/Danny/apache-ant-1.10.1/bin :/Library/TeX/texbin:/opt/X11/bin
+cd /Users/Danny/Documents/CS/NAS
+echo $PATH > ~/cronpath.txt
 python3 mail.py
 if [ -f signal_out ]; then
     a=$(cat signal_out)
     python3 drive.py --source $a
+    rm signal_out
 fi
